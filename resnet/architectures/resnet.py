63c8c1b76c996da44b27918f0c64720a48bc0f11
lucaslingle <lucasdaxlingle@gmail.com>
2021-12-20 02:15:03 -0800

    commit updates

--- resnet/architectures/resnet.py
+++ resnet/architectures/resnet.py
@@ -10,7 +10,7 @@ import torch as tc
 from residual_block import ResidualBlock
 
 
-def extract_ints(text, num):
+def extract_ints(text: str, num: int) -> Tuple[int]:
     pattern = r"(\w+)" + r",".join([r"([0-9]+)" for _ in range(num)])
     m = re.match(pattern, text)
     ints = tuple(map(lambda x: int(x), m.groups()[1:]))
@@ -20,7 +20,6 @@ def extract_ints(text, num):
 class ResNet(tc.nn.Module):
     def __init__(
             self,
-            img_shape: Tuple[int],
             architecture_spec: str,
             channels: int,
             preact: bool,
@@ -32,7 +31,7 @@ class ResNet(tc.nn.Module):
 
         :param architecture_spec: A string containing a space-separated list of
             architectural components chosen from
-                {"cK,S,P", "mpK,S,P", "apK,S,P", "rD", "bD", "n", "a", "fI,O}.
+                {"cK,S,P,I,O", "mpK,S,P", "apK,S,P", "rD", "bD", "n", "a", "fI,O}.
             c stands for convolution,
             mp for max pooling,
             ap for average pooling,
@@ -46,68 +45,92 @@ class ResNet(tc.nn.Module):
                 If two stacks of residual blocks are listed adjacently,
                 the first block in the second stack always performs downsampling.
             I,O stand for input, output dim.
-        :param: img_shape: Tuple containing image channels, height, width.
-        :param channels: Number of output channels in first convolution.
         :param preact: Use preactivation ordering?
         :param use_proj: Use projection on skip connection when downsampling?
         :param dropout_prob: Dropout probability.
         """
         super().__init__()
         self._architecture_spec = architecture_spec
-        self._img_shape = img_shape
         self._channels = channels
         self._preact = preact
         self._use_proj = use_proj
         self._dropout_prob = dropout_prob
 
-        module_list = []
-        channels = self._channels
-        for i, component in enumerate(self._architecture_spec.split(" ")):
-            if component.startswith('c'):
-                k, s, p = extract_ints(component, 3)
-                module = tc.nn.Conv2d(
-                    in_channels=channels if i > 0 else self._img_shape[0],
-                    out_channels=channels,
-                    kernel_size=(k,k),
-                    stride=(s,s),
-                    padding=(p,p))
-            elif component.startswith('mp'):
-                k, s, p = extract_ints(component, 3)
-                module = tc.nn.MaxPool2d(
-                    kernel_size=(k, k),
-                    stride=(s, s),
-                    padding=(p, p))
-            elif component.startswith('ap'):
-                k, s, p = extract_ints(component, 3)
-                module = tc.nn.AvgPool2d(
-                    kernel_size=(k, k),
-                    stride=(s, s),
-                    padding=(p, p))
-            elif component.startswith('r'):
-                assert i != 0
-                d = self._architecture_spec.split(" ")[i-1].startswith('r')
-                module = ResidualBlock(
-                    channels=channels,
-                    downsample=d,
-                    preact=self._preact,
-                    use_proj=self._use_proj,
-                    dropout_prob=self._dropout_prob)
-            elif component.startswith('b'):
-                assert i != 0
-                raise NotImplementedError
-            elif component.startswith('n'):
-                module = tc.nn.BatchNorm2d(
-                    num_features=channels)
-            elif component.startswith('a'):
-                module = tc.nn.ReLU()
-            elif component.startswith('f'):
-                i, o = extract_ints(component, 2)
-                module = tc.nn.Linear(i, o)
-            else:
-                raise NotImplementedError
-            module_list.append(module)
+        self._architecture = self._parse_spec(architecture_spec)
 
-        self._architecture = tc.nn.ModuleList(module_list)
+    def _make_conv(self, k, s, p, i, o):
+        return tc.nn.Conv2d(
+            in_channels=i,
+            out_channels=o,
+            kernel_size=(k, k),
+            stride=(s, s),
+            padding=(p, p))
+
+    def _make_avgpool(self, k, s, p):
+        return tc.nn.AvgPoool2d(
+            kernel_size=(k, k),
+            stride=(s, s),
+            padding=(p, p))
+
+    def _make_maxpool(self, k, s, p):
+        return tc.nn.MaxPoool2d(
+            kernel_size=(k, k),
+            stride=(s, s),
+            padding=(p, p))
+
+    def _make_res_stack(self, d, i, o, l):
+        return tc.nn.Sequential(*[
+            ResidualBlock(
+                channels=i if ell == 0 else o,
+                downsample=d if ell == 0 else False,
+                preact=self._preact,
+                use_proj=self._use_proj,
+                dropout_prob=self._dropout_prob)
+            for ell in range(l)
+        ])
+
+    def _make_bottleneck_res_stack(self, d, i, o, l):
+        raise NotImplementedError
+
+    def _make_norm(self, i):
+        return tc.nn.BatchNorm2d(num_features=i)
+
+    def _make_act(self):
+        return tc.nn.ReLU()
+
+    def _parse_spec(self, spec: str) -> tc.nn.Sequential:
+        ms = list()
+        channels = None
+        prev_component = None
+        for component in spec.split(" "):
+            if component.startswith('c'):
+                kspio = extract_ints(component, 5)
+                m = self._make_conv(*kspio)
+                channels = kspio[-1]
+            elif component.startswith('mp'):
+                m = self._make_maxpool(*extract_ints(component, 3))
+            elif component.startswith('ap'):
+                m = self._make_avgpool(*extract_ints(component, 3))
+            elif component.startswith('r'):
+                d = prev_component.startswith('r')
+                i = channels
+                o = channels if not d else 2 * channels
+                l = extract_ints(component, 1)
+                m = self._make_res_stack(d, i, o, l)
+                channels = o
+            elif component.startswith('b'):
+                d = prev_component.startswith('r')
+                i = channels
+                o = channels if not d else 2 * channels
+                l = extract_ints(component, 1)
+                m = self._make_bottleneck_res_stack(d, i, o, l)
+                channels = o
+            elif component.startswith('n'):
+                m = self._make_norm(i)
+            elif component.startswith('a'):
+                m = self._make_act()
+            ms.append(m)
+        return tc.nn.Sequential(*ms)
 
     def forward(self, x):
         return self._architecture(x)
