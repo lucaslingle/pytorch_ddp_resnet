e0486720ebff8921cef3e5dc1a7b636db8e70249
lucaslingle <lucasdaxlingle@gmail.com>
2021-12-21 14:43:25 -0800

    commit updates

--- resnet/utils/types_util.py
+++ resnet/utils/types_util.py
@@ -9,6 +9,6 @@ import torch as tc
 
 Module = tc.nn.Module
 Optimizer = tc.optim.Optimizer
-Scheduler = tc.optim.lr_scheduler._LRScheduler
+Scheduler = Union[tc.optim.lr_scheduler._LRScheduler, tc.optim.lr_scheduler.ReduceLROnPlateau]
 Checkpointable = Union[Module, Optimizer, Scheduler]
 Dataloader = tc.utils.data.DataLoader
