99b60ff6fa530a7500e5e36578e6ddfd194bda36
lucaslingle <lucasdaxlingle@gmail.com>
2021-12-21 01:18:43 -0800

    commit updates

--- resnet/utils/lr_util.py
+++ resnet/utils/lr_util.py
@@ -2,10 +2,19 @@
 Learning rate scheduler util.
 """
 
+from typing import Optional, Dict, Any
 import importlib
 
+import torch as tc
 
-def get_scheduler(scheduler_cls_name, optimizer, scheduler_args):
+
+def get_scheduler(
+        scheduler_cls_name: Optional[str],
+        optimizer: tc.optim.Optimizer,
+        scheduler_args: Dict[str, Any]
+):
+    if scheduler_cls_name == 'None':
+        return None
     module = importlib.import_module('torch.optim.lr_scheduler')
     scheduler_cls = getattr(module, scheduler_cls_name)
     return scheduler_cls(optimizer, **scheduler_args)
