5f11c645ba76c22476de7bfc471b7d4c702cb34b
lucaslingle <lucasdaxlingle@gmail.com>
2021-12-24 02:22:59 -0800

    commit updates

--- resnet/algos/metrics.py
+++ resnet/algos/metrics.py
@@ -5,10 +5,23 @@ Metrics and losses.
 import torch as tc
 
 
+def cross_entropy_loss(logits, labels):
+    return tc.nn.CrossEntropyLoss()(input=logits, target=labels)
+
+
+def top_k_err(logits, labels, k):
+    topk_preds = tc.topk(logits, k=k, dim=-1).indices
+    matches = tc.eq(topk_preds, labels.unsqueeze(-1)).float().sum(dim=-1)
+    acc_at_k = matches.mean()
+    return 1. - acc_at_k
+
+
 def compute_losses_and_metrics(logits, labels):
-    loss = tc.nn.CrossEntropyLoss()(input=logits, target=labels)
-    acc = tc.eq(logits.argmax(dim=-1), labels).float().mean()
+    loss = cross_entropy_loss(logits, labels)
+    top1 = top_k_err(logits, labels, k=1)
+    top5 = top_k_err(logits, labels, k=5)
     return {
         "loss": loss,
-        "acc": acc
+        "top1": top1,
+        "top5": top5
     }
