6b6a9471118c18be92bbf1e464d842c81a1742fe
lucaslingle <lucasdaxlingle@gmail.com>
2021-12-26 21:14:50 -0800

    commit updates

--- script.py
+++ script.py
@@ -60,6 +60,7 @@ def setup(rank, config):
     dataloaders = get_dataloaders(**config, **datasets, **samplers)
 
     device = f"cuda:{rank}" if tc.cuda.is_available() else "cpu"
+    scaler = tc.cuda.amp.GradScaler() if tc.cuda.is_available() else None
     classifier = tc.nn.parallel.DistributedDataParallel(
         ResNet(
             architecture_spec=config.get('architecture_spec'),
@@ -68,7 +69,6 @@ def setup(rank, config):
             dropout_prob=config.get('dropout_prob')
         ).to(device)
     )
-    scaler = tc.cuda.amp.GradScaler() if tc.cuda.is_available() else None
     optimizer = get_optimizer(
         model=classifier,
         optimizer_cls_name=config.get('optimizer_cls_name'),
