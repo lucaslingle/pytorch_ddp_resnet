aba5a97b58e9355242d3cdff77c601b78e4b8b18
lucaslingle <lucasdaxlingle@gmail.com>
2022-01-01 01:13:56 -0800

    commit updates

--- resnet/algos/training.py
+++ resnet/algos/training.py
@@ -95,7 +95,10 @@ def training_loop(
                 logits = classifier(x)
                 metrics = compute_losses_and_metrics(logits=logits, labels=y)
                 loss = metrics.get('loss')
-            scaler.scale(loss).backward() if scaler else loss.backward()
+            if scaler:
+                scaler.scale(loss).backward()
+            else:
+                loss.backward()
             global_metrics += global_means(metrics, world_size)
 
             if batch_done(microbatch_id):
